fa50e4c8e5965721a9dab151eb0db14e8337bc99
Степченко Александр Сергеевич <asstepchenko@edu.hse.ru>
2024-02-29 03:58:48 +0300

    fix stuff

--- 2/solution.c
+++ 2/solution.c
@@ -25,16 +25,9 @@ execute_command_line(const struct command_line *line)
     /* REPLACE THIS CODE WITH ACTUAL COMMAND EXECUTION */
 
     assert(line != NULL);
-    printf("================================\n");
-    printf("Command line:\n");
-    printf("Is background: %d\n", (int)line->is_background);
-    printf("Output: ");
-
     int output_fd = STDOUT_FILENO;
     if (line->out_type == OUTPUT_TYPE_STDOUT) {
-        printf("stdout\n");
     } else if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
-        printf("new file - \"%s\"\n", line->out_file);
         if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
             output_fd = open(line->out_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
             if (output_fd == -1) {
@@ -43,7 +36,6 @@ execute_command_line(const struct command_line *line)
             }
         }
     } else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
-        printf("append file - \"%s\"\n", line->out_file);
         if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
             output_fd = open(line->out_file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
             if (output_fd == -1) {
@@ -57,23 +49,16 @@ execute_command_line(const struct command_line *line)
 
 
     int (*pipes)[2] = NULL;
-    size_t num_pipes = 0;
-    size_t command_index = 0;
+    int num_pipes = 0;
 
     pid_t *child_pids = NULL;
-    size_t num_chidren = 0;
+    int num_children = 0;
 
     const struct expr *prev_e = NULL;
 
-    printf("Expressions:\n");
     const struct expr *e = line->head;
     while (e != NULL) {
         if (e->type == EXPR_TYPE_COMMAND) {
-            printf("\tCommand: %s", e->cmd.exe);
-            for (uint32_t i = 0; i < e->cmd.arg_count; ++i)
-                printf(" %s", e->cmd.args[i]);
-            printf("\n");
-
             // TODO: fix for && and ||
             if (strcmp(e->cmd.exe, "cd") == 0 && !e->next) {
                 chdir(e->cmd.args[0]);
@@ -81,14 +66,15 @@ execute_command_line(const struct command_line *line)
             }
 
             // TODO: fix for && and ||
-            if (strcmp(e->cmd.exe, "exit") == 0 && !e->next) {
-                struct execute_cmd_result result = {COMMAND_EXIT, -1};
+            if (strcmp(e->cmd.exe, "exit") == 0 && line->head == e && !e->next) {
+                struct execute_cmd_result result = {COMMAND_EXIT, EXIT_SUCCESS};
+                if (e->cmd.arg_count == 1) {
+                    result.status = atoi(e->cmd.args[0]);
+                }
                 return result;
             }
 
-            ++command_index;
-
-            child_pids = realloc(child_pids, ++num_chidren * sizeof(*child_pids));
+            child_pids = realloc(child_pids, ++num_children * sizeof(*child_pids));
 
             if (e->next && e->next->type == EXPR_TYPE_PIPE) {
                 pipes = realloc(pipes, ++num_pipes * sizeof(*pipes));
@@ -98,18 +84,26 @@ execute_command_line(const struct command_line *line)
                 }
             }
 
-
-            child_pids[num_chidren - 1] = fork();
-            if (child_pids[num_chidren - 1] == -1) {
+            child_pids[num_children - 1] = fork();
+            if (child_pids[num_children - 1] == -1) {
                 perror("fork");
                 exit(EXIT_FAILURE);
             }
 
-            if (child_pids[num_chidren - 1] == 0) {
+            if (child_pids[num_children - 1] == 0) {
+                // TODO: fix for && and ||
+                if (strcmp(e->cmd.exe, "exit") == 0) {
+                    if (e->cmd.arg_count == 1) {
+                        exit(atoi(e->cmd.args[0]));
+                    } else {
+                        exit(EXIT_SUCCESS);
+                    }
+                }
+
                 // Close the pipes
                 bool is_next_pipe = e->next && e->next->type == EXPR_TYPE_PIPE;
-                size_t reading_pipe_index = is_next_pipe ? num_pipes - 2 : num_pipes - 1;
-                for (size_t j = 0; j < num_pipes; ++j) {
+                int reading_pipe_index = is_next_pipe ? num_pipes - 2 : num_pipes - 1;
+                for (int j = 0; j < num_pipes; ++j) {
                     if (j != reading_pipe_index && j != num_pipes - 1) {
                         close(pipes[j][0]);
                         close(pipes[j][1]);
@@ -140,24 +134,26 @@ execute_command_line(const struct command_line *line)
                 if (!e->next && (line->out_type == OUTPUT_TYPE_FILE_NEW || line->out_type == OUTPUT_TYPE_FILE_APPEND)) {
                     int new_fd = dup2(output_fd, STDOUT_FILENO);
                     if (new_fd == -1) {
-                        perror("dup2 out file");
+                        perror("dup2");
                         exit(EXIT_FAILURE);
                     }
                 }
 
-                char **argv = malloc((e->cmd.arg_count + 1) * sizeof(*argv));
+                char **argv = malloc((e->cmd.arg_count + 2) * sizeof(*argv));
                 argv[0] = e->cmd.exe;
                 for (size_t i = 0; i < e->cmd.arg_count; ++i) {
                     argv[i + 1] = e->cmd.args[i];
                 }
-                execvp(e->cmd.exe, argv);
+                argv[e->cmd.arg_count + 1] = NULL;
+                int err = execvp(e->cmd.exe, argv);
+                if (err == -1) {
+                    perror("execvp");
+                    exit(EXIT_FAILURE);
+                }
             }
         } else if (e->type == EXPR_TYPE_PIPE) {
-            printf("\tPIPE\n");
         } else if (e->type == EXPR_TYPE_AND) {
-            printf("\tAND\n");
         } else if (e->type == EXPR_TYPE_OR) {
-            printf("\tOR\n");
         } else {
             assert(false);
         }
@@ -165,7 +161,7 @@ execute_command_line(const struct command_line *line)
         e = e->next;
     }
 
-    for (size_t i = 0; i < num_pipes; ++i) {
+    for (int i = 0; i < num_pipes; ++i) {
         close(pipes[i][0]);
         close(pipes[i][1]);
     }
@@ -173,16 +169,9 @@ execute_command_line(const struct command_line *line)
 
     int wstatus;
     int exit_status = -1;
-    printf("num_children == %zu\n", num_chidren);
-    for (size_t i = 0; i < num_chidren; ++i) {
+    for (int i = 0; i < num_children; ++i) {
         waitpid(child_pids[i], &wstatus, 0);
         exit_status = WEXITSTATUS(wstatus);
-        if (WIFEXITED(wstatus)) {
-            printf("parent: child_%zu exited normally with status %d\n", i, exit_status);
-        }
-        if (WIFSIGNALED(wstatus)) {
-            printf("parent: child_%zu was terminated by signal %d\n", i, WTERMSIG(wstatus));
-        }
     }
     free(child_pids);
 
@@ -198,11 +187,10 @@ main(void)
     int rc;
     struct parser *p = parser_new();
 
-    write(STDOUT_FILENO, "> ", 2);
+    struct execute_cmd_result result = {COMMAND_EXIT, EXIT_FAILURE};
     while ((rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
         parser_feed(p, buf, rc);
         struct command_line *line = NULL;
-        struct execute_cmd_result result;
         while (true) {
             enum parser_error err = parser_pop_next(p, &line);
             if (err == PARSER_ERR_NONE && line == NULL)
@@ -217,8 +205,7 @@ main(void)
         if (result.type == COMMAND_EXIT) {
             break;
         }
-        write(STDOUT_FILENO, "> ", 2);
     }
     parser_delete(p);
-    return 0;
+    return result.status;
 }
