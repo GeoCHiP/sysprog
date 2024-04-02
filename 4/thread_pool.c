1cfb72b79e59be024de9c4faa43f93e2d01770d2
Степченко Александр Сергеевич <asstepchenko@edu.hse.ru>
2024-04-03 00:51:36 +0300

    feat: start implementing thread pool

--- 4/thread_pool.c
+++ 4/thread_pool.c
@@ -1,86 +1,126 @@
 #include "thread_pool.h"
+#include <assert.h>
+#include <errno.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum task_state {
+	IS_NEW = 0,
+	IS_PUSHED,
+	IS_RUNNING,
+	IS_FINISHED
+};
 
 struct thread_task {
 	thread_task_f function;
 	void *arg;
 
 	/* PUT HERE OTHER MEMBERS */
+	enum task_state state;
+	void *result;
+
+	struct thread_task *next;
 };
 
 struct thread_pool {
 	pthread_t *threads;
 
 	/* PUT HERE OTHER MEMBERS */
+	int threads_capacity;
+	struct thread_task *tasks_queue_head;
+	struct thread_task *tasks_queue_tail;
+	int queue_size;
 };
+
+#define handle_error_en(en, msg) \
+	   do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
 int
 thread_pool_new(int max_thread_count, struct thread_pool **pool)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)max_thread_count;
-	(void)pool;
-	return TPOOL_ERR_NOT_IMPLEMENTED;
+	if (max_thread_count < 1 || max_thread_count > TPOOL_MAX_THREADS) {
+		return TPOOL_ERR_INVALID_ARGUMENT;
+	}
+	*pool = malloc(sizeof(**pool));
+	memset(*pool, 0, sizeof(**pool));
+	return 0;
 }
 
 int
 thread_pool_thread_count(const struct thread_pool *pool)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)pool;
-	return TPOOL_ERR_NOT_IMPLEMENTED;
+	return pool->threads_capacity;
 }
 
 int
 thread_pool_delete(struct thread_pool *pool)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)pool;
-	return TPOOL_ERR_NOT_IMPLEMENTED;
+	// TODO: check for active tasks
+	free(pool);
+	return 0;
 }
 
 int
 thread_pool_push_task(struct thread_pool *pool, struct thread_task *task)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)pool;
-	(void)task;
-	return TPOOL_ERR_NOT_IMPLEMENTED;
+	if (pool->queue_size == TPOOL_MAX_TASKS) {
+		return TPOOL_ERR_TOO_MANY_TASKS;
+	}
+
+	task->state = IS_RUNNING;
+	int err = pthread_create(&pool->threads[0], NULL, task->function, task->arg);
+	if (err != 0) {
+		handle_error_en(err, "pthread_create");
+	}
+
+	/*if (pool->queue_size == 0) {*/
+		/*pool->tasks_queue_head = task;*/
+		/*pool->tasks_queue_tail = task;*/
+	/*} else {*/
+		/*pool->tasks_queue_tail->next = task;*/
+	/*}*/
+	/*task->state = IS_PUSHED;*/
+
+	return 0;
 }
 
 int
 thread_task_new(struct thread_task **task, thread_task_f function, void *arg)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)task;
-	(void)function;
-	(void)arg;
-	return TPOOL_ERR_NOT_IMPLEMENTED;
+	*task = malloc(sizeof(**task));
+	(*task)->function = function;
+	(*task)->arg = arg;
+	return 0;
 }
 
 bool
 thread_task_is_finished(const struct thread_task *task)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)task;
-	return false;
+	return task->state == IS_FINISHED;
 }
 
 bool
 thread_task_is_running(const struct thread_task *task)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)task;
-	return false;
+	return task->state == IS_RUNNING;
 }
 
 int
 thread_task_join(struct thread_task *task, void **result)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)task;
-	(void)result;
-	return TPOOL_ERR_NOT_IMPLEMENTED;
+	if (task->state == IS_NEW) {
+		return TPOOL_ERR_TASK_NOT_PUSHED;
+	}
+	// TODO: maybe use condition variable for this;
+	while (task->state != IS_FINISHED) {
+		// TODO: pthread_cond_wait
+	}
+
+	*result = task->result;
+
+	return 0;
 }
 
 #ifdef NEED_TIMED_JOIN
@@ -100,9 +140,11 @@ thread_task_timed_join(struct thread_task *task, double timeout, void **result)
 int
 thread_task_delete(struct thread_task *task)
 {
-	/* IMPLEMENT THIS FUNCTION */
-	(void)task;
-	return TPOOL_ERR_NOT_IMPLEMENTED;
+	if (task->state != IS_NEW) {
+		return TPOOL_ERR_TASK_IN_POOL;
+	}
+	free(task);
+	return 0;
 }
 
 #ifdef NEED_DETACH
